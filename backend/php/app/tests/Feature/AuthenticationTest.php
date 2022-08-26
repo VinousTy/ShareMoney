fbbebb0ebecc3d43d4b08ba7caea7e25ca9de800
github VinousTy <github ys.taka44@gmail.com>
2022-08-26 11:03:57 +0900

    Add:認証周りの処理をテスト

---
+++ backend/php/app/tests/Feature/AuthenticationTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class AuthenticationTest extends TestCase
+{
+  use RefreshDatabase;
+
+  public function setUp(): void
+  {
+    parent::setUp();
+
+    $this->user = User::create([
+      'email' => 'dummy@main.com',
+      'password' => bcrypt('password'),
+    ]);
+  }
+
+  public function testLogin()
+  {
+    $response = $this->json('POST', route('login'), [
+      'email' => 'dummy@main.com',
+      'password' => 'password',
+    ]);
+
+    $response->assertStatus(200);
+
+    $response->assertJson(
+      fn (AssertableJson $json) =>
+      $json->whereType('access_token', 'string')
+        ->whereType('message', 'string')
+        ->etc()
+    );
+
+    $this->assertAuthenticatedAs($this->user);
+  }
+}
