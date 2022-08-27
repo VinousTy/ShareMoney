f3e5d7c4b3120c609e061eb194aa3e3b43706607
github VinousTy <github ys.taka44@gmail.com>
2022-08-27 17:47:04 +0900

    Update:パスワードリセット通信を行う際にbackendへ送信するデータ型を定義

--- frontend/app/src/types/Types.ts
+++ frontend/app/src/types/Types.ts
@@ -5,6 +5,8 @@ export interface File extends Blob {
 
 export interface AUTH_STATE {
   signIn: boolean;
+  isEmail: boolean;
+  isNotEmail: boolean;
   user: {
     id: number;
     email: string;
@@ -16,6 +18,12 @@ export interface AUTH_STATE {
 export interface AUTH_DATA {
   email: string;
   password: string;
+}
+
+export interface NEW_PASSWORD {
+  email: string | null;
+  password: string;
+  token: string | null;
 }
 
 export interface LAYOUT_STATE {
