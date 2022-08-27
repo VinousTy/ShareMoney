cfa5285a6512bd1a251d4e11a7356b2ab3072c14
github VinousTy <github ys.taka44@gmail.com>
2022-08-27 17:45:03 +0900

    Add:パスワードリセットする通信処理を記述

--- frontend/app/src/features/auth/authSlice.ts
+++ frontend/app/src/features/auth/authSlice.ts
@@ -1,12 +1,14 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { RootState } from '../../app/store';
-import { AUTH_STATE, AUTH_DATA } from '../../types/Types';
+import { AUTH_STATE, AUTH_DATA, NEW_PASSWORD } from '../../types/Types';
 
 const apiUrl = process.env.REACT_APP_DEV_API_URL;
 
 const initialState: AUTH_STATE = {
   signIn: false,
+  isEmail: false,
+  isNotEmail: false,
   user: {
     id: 0,
     email: '',
@@ -34,6 +36,34 @@ export const registUser = createAsyncThunk(
   }
 );
 
+export const postEmail = createAsyncThunk(
+  'auth/email',
+  async (data: { email: string }) => {
+    const res = await axios.post(`${apiUrl}api/password/request`, data, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return res.data;
+  }
+);
+
+export const newPassword = createAsyncThunk(
+  'new/password',
+  async (data: NEW_PASSWORD) => {
+    const res = await axios.post(
+      `${apiUrl}api/password/reset/${data.token}`,
+      data,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    return res.data;
+  }
+);
+
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
@@ -51,13 +81,32 @@ export const authSlice = createSlice({
       state.successOrFailure = action.payload;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(postEmail.fulfilled, (state, action) => {
+      state.isEmail = true;
+      state.isNotEmail = false;
+    });
+    builder.addCase(postEmail.rejected, (state, action) => {
+      state.isEmail = false;
+      state.isNotEmail = true;
+    });
+    builder.addCase(newPassword.fulfilled, (state, action) => {
+      state.message = action.payload;
+      state.successOrFailure = true;
+    });
+    builder.addCase(newPassword.rejected, (state, action) => {
+      state.message = action.payload;
+      state.successOrFailure = false;
+    });
+  },
 });
 
 export const { isSignIn, isSignOut, editMessage, isSuccessOrFailure } =
   authSlice.actions;
 
 export const selectIsSignIn = (state: RootState) => state.auth.signIn;
+export const selectIsNotEmail = (state: RootState) => state.auth.isNotEmail;
+export const selectIsEmail = (state: RootState) => state.auth.isEmail;
 export const selectUserId = (state: RootState) => state.auth.user.id;
 export const selectMessage = (state: RootState) => state.auth.message;
 export const selectSuccessOrFailure = (state: RootState) =>
