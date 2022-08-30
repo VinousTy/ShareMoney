5f58c1dca495b31181d0fcb5d693dbb1f8eee953
github VinousTy <github ys.taka44@gmail.com>
2022-08-30 18:16:04 +0900

    Change:idの指定方法を変更

--- backend/php/app/database/seeders/UserSeeder.php
+++ backend/php/app/database/seeders/UserSeeder.php
@@ -18,7 +18,7 @@ class UserSeeder extends Seeder
   {
     DB::table('users')->insert([
       [
-        'id' => '8f2ce26a-8923-467c-b844-fae0bfa1b20d',
+        'id' => Str::uuid(),
         'email' => 'guest@example.com',
         'password' => Hash::make(env('GUEST_PASSWORD')),
         'created_at' => '2022/08/07 14:09:10'
