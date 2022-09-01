a5c3d15e0742fad77426d3fc629f681b1cd3edae
github VinousTy <github ys.taka44@gmail.com>
2022-09-01 10:35:29 +0900

    Add:PostAccountBookモデルの作成

---
+++ backend/php/app/app/Models/PostAccountBook.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class PostAccountBook extends Model
+{
+  use HasFactory;
+
+  protected $fillable = [
+    'id',
+    'date',
+    'monthly_income',
+    'user_id'
+  ];
+
+  public $incrementing = false;
+
+  protected $keyType = 'string';
+
+  protected static function booted()
+  {
+    static::creating(function (PostAccountBook $model) {
+      empty($model->id) && $model->id = Str::uuid();
+    });
+  }
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+
+  public function expenses()
+  {
+    return $this->hasMany(PostExpense::class);
+  }
+}
