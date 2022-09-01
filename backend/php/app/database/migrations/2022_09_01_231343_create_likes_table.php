9b787255c1584d9a74880019da660b7e1b890c5c
github VinousTy <github ys.taka44@gmail.com>
2022-09-02 08:38:05 +0900

    Add:likesテーブルのmigrationファイルを作成

---
+++ backend/php/app/database/migrations/2022_09_01_231343_create_likes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('likes', function (Blueprint $table) {
+      $table->uuid('id')->primary();
+      $table->uuid('user_id');
+      $table->foreign('user_id')
+        ->references("id")
+        ->on("users")
+        ->onUpdate('cascade')
+        ->onDelete('cascade');
+      $table->uuid('post_account_book_id');
+      $table->foreign('post_account_book_id')
+        ->references("id")
+        ->on("post_account_books")
+        ->onUpdate('cascade')
+        ->onDelete('cascade');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('likes');
+  }
+}
