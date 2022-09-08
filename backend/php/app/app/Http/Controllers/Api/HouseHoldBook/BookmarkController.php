3a65c33a70011a3ebb38842f567a01b3b3c17631
github VinousTy <github ys.taka44@gmail.com>
2022-09-08 10:14:11 +0900

    Update:Bookmarkした投稿のみ返却する関数を作成

--- backend/php/app/app/Http/Controllers/Api/HouseHoldBook/BookmarkController.php
+++ backend/php/app/app/Http/Controllers/Api/HouseHoldBook/BookmarkController.php
@@ -6,9 +6,30 @@ use App\Http\Controllers\Controller;
 use App\Models\Bookmark;
 use App\Models\PostAccountBook;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BookmarkController extends Controller
 {
+  public function index()
+  {
+    $bookmarkAccountBooks = PostAccountBook::with(['likes', 'bookmarks'])
+      ->join('bookmarks', 'post_account_books.id', '=', 'bookmarks.post_account_book_id')
+      ->where('bookmarks.user_id', Auth::id())
+      ->get(['post_account_books.id', 'post_account_books.user_id', 'post_account_books.date', 'post_account_books.monthly_income']);
+
+    $bookmarkCosts = Bookmark::join('post_account_books', 'post_account_books.id', '=', 'bookmarks.post_account_book_id')
+      ->join('post_expenses', 'post_account_books.id', '=', 'post_expenses.post_account_book_id')
+      ->selectRaw('post_account_books.date ,post_expenses.expenseItem, post_account_books.user_id, sum(post_expenses.cost) as cost')
+      ->where('bookmarks.user_id', Auth::id())
+      ->groupBy('post_account_books.date', 'post_expenses.expenseItem', 'post_account_books.user_id')
+      ->get();
+
+    return response()->json([
+      'accountBook' => $bookmarkAccountBooks,
+      'costs' =>  $bookmarkCosts
+    ], 200);
+  }
+
   public function bookmark(Request $request)
   {
     Bookmark::create([
