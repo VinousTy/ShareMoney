081f0991001fec92feaf652d6d5907531bd5c5be
github VinousTy <github ys.taka44@gmail.com>
2022-09-16 11:11:00 +0900

    Update:ローカルスコープを定義

--- backend/php/app/app/Models/Profile.php
+++ backend/php/app/app/Models/Profile.php
@@ -37,4 +37,39 @@ class Profile extends Model
   {
     return $this->belongsTo(User::class);
   }
+
+  public function scopeJoinUsers($query)
+  {
+    return $query->join('users', 'profiles.user_id', '=', 'users.id');
+  }
+
+  public function scopeJoinPostAccountBooks($query)
+  {
+    return $query->join('post_account_books', 'users.id', '=', 'post_account_books.user_id');
+  }
+
+  public function scopeJoinPostExpenses($query)
+  {
+    return $query->join('post_expenses', 'post_account_books.id', '=', 'post_expenses.post_account_book_id');
+  }
+
+  public function scopeSearchName($query, $name)
+  {
+    return $query->where('profiles.name', '=', $name);
+  }
+
+  public function scopeSearchIncome($query, $income)
+  {
+    return $query->where('profiles.income', '=', $income);
+  }
+
+  public function scopeSearchJob($query, $job)
+  {
+    return $query->where('profiles.job', '=', $job);
+  }
+
+  public function scopeSearchComposition($query, $composition)
+  {
+    return $query->where('profiles.composition', '=', $composition);
+  }
 }
