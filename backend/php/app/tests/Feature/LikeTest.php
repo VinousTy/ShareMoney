d27e385eae15c68b1d94058e114a16a42e7d3577
github VinousTy <github ys.taka44@gmail.com>
2022-09-18 09:55:57 +0900

    Update:中間テーブルのテスト処理を修正

--- backend/php/app/tests/Feature/LikeTest.php
+++ backend/php/app/tests/Feature/LikeTest.php
@@ -7,6 +7,7 @@ use App\Models\PostAccountBook;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
@@ -23,34 +24,7 @@ class LikeTest extends TestCase
     $response->assertStatus(302);
   }
 
-  public function testLikeCreate()
-  {
-    $user = User::factory()->create();
-
-    $auth = $this->actingAs($user);
-
-    $postAccountBook = PostAccountBook::factory()->create();
-
-    $response = $this->post(
-      route('like/create'),
-      [
-        'user_id' => $user->id,
-        'post_account_book_id' => $postAccountBook->id,
-      ]
-    );
-
-    $response->assertStatus(200);
-    $this->assertEquals(1, Like::count());
-
-    $response->assertJson(
-      fn (AssertableJson $json) =>
-      $json->whereType('accountBook', 'array')
-        ->whereType('income', 'array')
-        ->whereType('costs', 'array')
-    );
-  }
-
-  public function testLikeDestroy()
+  public function testLike()
   {
     $user = User::factory()->create();
 
