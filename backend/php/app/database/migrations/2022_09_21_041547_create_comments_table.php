c123ed469e5d8c9196a0ec2944cef15e63ae2a6e
github VinousTy <github ys.taka44@gmail.com>
2022-09-21 17:55:24 +0900

    Add:commentsテーブルのmigrationファイルを作成

---
+++ backend/php/app/database/migrations/2022_09_21_041547_create_comments_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('comments', function (Blueprint $table) {
+      $table->id();
+      $table->string('body');
+      $table->uuid('user_id');
+      $table->foreign('user_id')
+        ->references("id")
+        ->on("users")
+        ->onUpdate('cascade')
+        ->onDelete('cascade');
+      $table->uuid('post_account_book_id');
+      $table->foreign('post_account_book_id')
+        ->references("id")
+        ->on("post_account_books")
+        ->onUpdate('cascade')
+        ->onDelete('cascade');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('comments');
+  }
+}
