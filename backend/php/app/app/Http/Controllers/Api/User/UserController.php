f45c4090d2ef6857c09856393a45d7d5c7baa736
github VinousTy <github ys.taka44@gmail.com>
2022-11-12 17:54:33 +0900

    Update:通知を取得・削除する処理を追加

--- backend/php/app/app/Http/Controllers/Api/User/UserController.php
+++ backend/php/app/app/Http/Controllers/Api/User/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Notification;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
@@ -10,5 +12,33 @@ class UserController extends Controller
   public function index()
   {
     return response()->json(Auth::user(), 200);
+  }
+
+  public function notify()
+  {
+    $user = Auth::user();
+    $notifyArray = [];
+
+    foreach ($user->notifications as $notification) {
+      array_push($notifyArray, $notification->data);
+    }
+
+    return response()->json($notifyArray, 200);
+  }
+
+  public function unNotify(Request $request)
+  {
+    $user = Auth::user();
+
+    $id = '';
+
+    foreach ($user->notifications as $notification) {
+      $id = Notification::where('data->id', $request->id)->get('id');
+    }
+
+    $notify = Notification::findOrFail($id[0]->id);
+    $notify->delete();
+
+    return response()->json('通知を確認しました。', 200);
   }
 }
