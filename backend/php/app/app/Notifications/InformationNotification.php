d743be8474225341956f05614cab12df492ec629
github VinousTy <github ys.taka44@gmail.com>
2022-11-12 18:34:03 +0900

    Add:コメントを通知する処理を行うクラスを作成

---
+++ backend/php/app/app/Notifications/InformationNotification.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\PostAccountBook;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Str;
+
+class InformationNotification extends Notification
+{
+  use Queueable;
+
+  /**
+   * Create a new notification instance.
+   *
+   * @return void
+   */
+  public function __construct($user, $postAccountBook, $comment)
+  {
+    $this->user = $user;
+    $this->postAccountBook = $postAccountBook;
+    $this->comment = $comment;
+  }
+
+  /**
+   * Get the notification's delivery channels.
+   *
+   * @param  mixed  $notifiable
+   * @return array
+   */
+  public function via($notifiable)
+  {
+    return ['database'];
+  }
+
+  /**
+   * Get the mail representation of the notification.
+   *
+   * @param  mixed  $notifiable
+   * @return \Illuminate\Notifications\Messages\MailMessage
+   */
+  // public function toMail($notifiable)
+  // {
+  //     return (new MailMessage)
+  //                 ->line('The introduction to the notification.')
+  //                 ->action('Notification Action', url('/'))
+  //                 ->line('Thank you for using our application!');
+  // }
+
+  /**
+   * Get the array representation of the notification.
+   *
+   * @param  mixed  $notifiable
+   * @return array
+   */
+  public function toArray($notifiable)
+  {
+    return [
+      'id' => Str::uuid(),
+      'user_id' => $this->user->id,
+      'post_account_id' => $this->postAccountBook->id,
+      'date' => $this->postAccountBook->date,
+      'created_at' => $this->comment->created_at
+    ];
+  }
+}
